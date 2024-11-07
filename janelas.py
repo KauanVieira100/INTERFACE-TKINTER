3cf49a6bc12d15c8f6b749c32f4ba834d3aaacb3
Kauan <kauan_a_vieira@estudante.sesisenai.org.br>
2024-11-07 11:46:57 -0300

    Add files via upload

---
+++ janelas.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+
+def abrir_janela():
+    janela2 = tk.Toplevel()
+    janela2.title('Janela nova')
+    botao_fechar = tk.Button(janela2, text = "Fechar", command = janela2.destroy)
+    botao_fechar.grid(row = 1, column = 0)
+
+
+
+    
+janela = tk.Tk()
+botao = tk.Button(janela, text = 'ir para outra janela', command = abrir_janela)
+botao.grid(row = 2, column = 3)
+
+janela.mainloop()
